f0f21f9eda1777bab8c107cc7fa9669ba3a2934f
eKoh95 <greatekoh@gmail.com>
2020-08-24 11:19:03 -0600

    Lesson 11: Operators precedence

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -29,58 +29,80 @@ var JohnMark = 'John and mark';
 /***************************
  * Variable mutation and type coercion
  */
+/*
+// Type coercion
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+var job, isMarried;
+job = 'teadher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+/***************************
+* Basic operators
+*/
 
- /*
- // Type coercion
- var firstName = 'John';
- var age = 28;
+/*
+// Math operators
+var year, yearJohn, yearMark;
 
- console.log(firstName + ' ' + age);
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+yearJohn = now - 28;
+yearMark = now - 33;
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+/***************************
+* Operators precedence
+*/
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
 
- var job, isMarried;
- job = 'teadher';
- isMarried = false;
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
 
- console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+console.log(isFullAge);
 
- // Variable mutation
- age = 'twenty eight';
- job = 'driver';
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = ( ageJohn + ageMark ) / 2;
+console.log( average );
 
- alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+// Multiple assignments
+var x, y;
+x = y = ( 3 + 5 ) * 4 - 6;
+console.log( x, y );
 
- var lastName = prompt('What is his last Name?');
- console.log(firstName + ' ' + lastName);
+// More operators
 
- */
-
- /***************************
- * Basic operators
- */
-
- // Math operators
- var year, yearJohn, yearMark;
- 
- now = 2018;
- ageJohn = 28;
- ageMark = 33;
-
- yearJohn = now - 28;
- yearMark = now - 33;
-
- console.log(yearJohn);
-
- console.log(now + 2);
- console.log(now * 2);
- console.log(now / 10);
-
- // Logical operators
- var johnOlder = ageJohn > ageMark;
- console.log(johnOlder);
-
- // typeof operator
- console.log(typeof johnOlder);
- console.log(typeof ageJohn);
- console.log(typeof 'Mark is older than John');
- var x;
- console.log(typeof x);
+// x = x * 2;
+x *= 2;
+console.log( x );
+x += 10;
+console.log( x );
+x++;
+console.log( x );
+x--;
+console.log( x );
