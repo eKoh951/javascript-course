69c00ccdafdeeb27c84b5af2904940fb9e090f40
eKoh95 <greatekoh@gmail.com>
2020-08-24 10:38:55 -0600

    Lesson 8: Data type, conventions and rules

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,20 @@
-console.log('Hello world!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// var 3years = 3;	// error with 3
+var _3years = 3;
+// var John/mark = 'John and mark'; // error with /
+var JohnMark = 'John and mark';
+// var if = 23; // error with if
