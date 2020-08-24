32900a40f91e0eae72f26f4913526b70cab3febd
eKoh95 <greatekoh@gmail.com>
2020-08-24 10:52:50 -0600

    Lesson 9: Variable mutation and type coercion

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,3 +1,8 @@
+/***************************
+ * Variables and data types
+ */
+
+ /*
 var firstName = 'John';
 console.log(firstName);
 
@@ -12,9 +17,36 @@ console.log(job);
 
 job = 'teacher';
 console.log(job);
+*/
 
+// Variable naming rules
 // var 3years = 3;	// error with 3
 var _3years = 3;
 // var John/mark = 'John and mark'; // error with /
 var JohnMark = 'John and mark';
 // var if = 23; // error with if
+
+/***************************
+ * Variable mutation and type coercion
+ */
+
+ // Type coercion
+ var firstName = 'John';
+ var age = 28;
+
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+ job = 'teadher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+ // Variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+ var lastName = prompt('What is his last Name?');
+ console.log(firstName + ' ' + lastName);
