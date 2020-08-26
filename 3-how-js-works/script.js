2058c369348b425ca04553a95fb4e882fa670cdd
eKoh95 <greatekoh@gmail.com>
2020-08-26 14:49:05 -0600

    Lesson 39: Hoisting in Practice

--- 3-how-js-works/script.js
+++ 3-how-js-works/script.js
@@ -1,9 +1,30 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calcAge( 1999 );
+
+function calcAge ( year ) {
+    console.log( 2016 - year );
+}
 
 
+// retirement( 1990 );
 
+var retirement = function( year ) {
+    console.log( 65 - ( 2016 - year) );
+}
+
+var age = 23;
+console.log( age );
+
+function foo() {
+    console.log( age );
+    var age = 65;
+    console.log( age );
+}
+
+foo();
+console.log( age );
 
 
 
