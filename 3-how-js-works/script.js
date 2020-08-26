f203937d65c3b949efff34a05be1c8a08ac44ddd
eKoh95 <greatekoh@gmail.com>
2020-08-26 15:09:19 -0600

    Scoping example

--- 3-how-js-works/script.js
+++ 3-how-js-works/script.js
@@ -1,35 +1,47 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-calcAge( 1999 );
+// // calcAge( 1999 );
 
-function calcAge ( year ) {
-    console.log( 2016 - year );
-}
+// // function calcAge ( year ) {
+// //     console.log( 2016 - year );
+// }
 
 
 // retirement( 1990 );
 
-var retirement = function( year ) {
-    console.log( 65 - ( 2016 - year) );
+// var retirement = function( year ) {
+//     console.log( 65 - ( 2016 - year) );
+// }
+
+// var age = 23;
+// console.log( age );
+
+// function foo() {
+//     console.log( age );
+//     var age = 65;
+//     console.log( age );
+// }
+
+// foo();
+// console.log( age );
+
+/****************************
+* Scoping example
+****************************/
+
+var a = 'Hello';
+first();
+
+function first() {
+    var b = 'Hi';
+    second();
+
+    function second() {
+        var c = 'Hey';
+        console.log( a + b + c );
+    }
 }
-
-var age = 23;
-console.log( age );
-
-function foo() {
-    console.log( age );
-    var age = 65;
-    console.log( age );
-}
-
-foo();
-console.log( age );
-
-
-
-
-
 
 
 
