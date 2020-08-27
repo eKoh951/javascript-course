35c5cfab4ff254830b318b09800e4a5b0715a236
eKoh95 <greatekoh@gmail.com>
2020-08-27 10:28:58 -0600

    Lesson 57: Dom Manipulation end

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -8,8 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scpres, roundScore, activePlayer, dice;
-const pointsToWin = 20;
+var scores, roundScore, activePlayer, dice, gamePlaying, lastDice, pointsToWin = 20;
 
 init();
 
@@ -19,42 +18,72 @@ function btn() {
 
 document.querySelector( '.btn-roll' ).addEventListener( 'click', function() {
 	
-	// 1. Random number
-	var dice = Math.floor( Math.random() * 6 ) + 1;
 	
-	// 2. Display the result
-	var diceDOM = document.querySelector( '.dice' );
-	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png';
-	
-	// 3. Update the round score IF the rolled number was NOT a 1
-	if( dice !== 1 ) {
-		// Add score
-		roundScore += dice;
-		document.querySelector( '#current-' + activePlayer ).textContent = roundScore;
-	} else {
-		nextPlayer();
+	if( gamePlaying ) {
+		// 1. Random number
+		var dice1 = Math.floor( Math.random() * 6 ) + 1;
+		var dice2 = Math.floor( Math.random() * 6 ) + 1;
+		
+		// 2. Display the result
+		document.getElementById( 'dice-1' ).style.display = 'block';
+		document.getElementById( 'dice-2' ).style.display = 'block';
+		document.getElementById( 'dice-1' ).src = 'dice-' + dice1 + '.png';
+		document.getElementById( 'dice-2' ).src = 'dice-' + dice2 + '.png';
+		
+		// if( dice === 6 && lastDice == 6 ) {
+		// 	lastDice = 0;
+		// 	scores[activePlayer] = 0;
+		// 	document.querySelector( '#current-' + activePlayer ).textContent = roundScore;
+		// 	nextPlayer();
+		// } else if ( dice !== 1 ) {
+		// 	// Add score
+		// 	roundScore += dice;
+		// 	document.querySelector( '#current-' + activePlayer ).textContent = roundScore;
+		// 	lastDice = dice;
+		// } else {
+		// 	nextPlayer();
+		// }
+
+		if( dice1 !== 1 && dice2 !== 1 ) {
+			var dice = dice1 + dice2;
+			roundScore += dice;
+			document.querySelector( '#current-' + activePlayer ).textContent = roundScore;
+		} else {
+			nextPlayer();
+		}
 	}
+
 
 } );
 
 document.querySelector( '.btn-hold' ).addEventListener( 'click', function() {
 	
-	// Add current score to global score
-	scores[activePlayer] += roundScore;
+	if( gamePlaying ) {
 
-	//Update the UI
-	document.querySelector( '#score-' + activePlayer ).textContent = scores[activePlayer];
-	
-	// Check if Player won the game
-	if( scores[activePlayer] >= pointsToWin ){
-		document.querySelector('#name-' + activePlayer ).textContent = 'Winner!';
-		document.querySelector( '.dice' ).style.display = 'none';
-		document.querySelector( '.player-' + activePlayer + '-panel' ).classList.add('winner');
-		document.querySelector( '.player-' + activePlayer + '-panel' ).classList.remove('active');
-	} else {
-		// Toggle active player
-		nextPlayer();
+		// Add current score to global score
+		scores[activePlayer] += roundScore;
+		
+		//Update the UI
+		document.querySelector( '#score-' + activePlayer ).textContent = scores[activePlayer];
+		
+		// Check if Player won the game
+		var inputScore = parseInt( document.querySelector( 'input' ).value );
+		
+		if( inputScore !== NaN ){
+			pointsToWin = inputScore;
+		}
+
+		if( scores[activePlayer] >= pointsToWin ) {
+			document.querySelector('#name-' + activePlayer ).textContent = 'Winner!';
+			document.getElementById( 'dice-1' ).style.display = 'none';
+			document.getElementById( 'dice-2' ).style.display = 'none';
+			document.querySelector( '.player-' + activePlayer + '-panel' ).classList.add('winner');
+			document.querySelector( '.player-' + activePlayer + '-panel' ).classList.remove('active');
+			gamePlaying = false;
+		} else {
+			// Toggle active player
+			nextPlayer();
+		}
 	}
 
 });
@@ -72,7 +101,8 @@ function nextPlayer() {
 
 		// document.querySelector( '.player-0-panel' ).classList.remove( 'active' );
 		// document.querySelector( '.player-1-panel' ).classList.add( 'active' );
-		document.querySelector( '.dice' ).style.display = 'none';
+		// document.getElementById( 'dice-1' ).style.display = 'none';
+		// document.getElementById( 'dice-2' ).style.display = 'none';
 }
 
 document.querySelector( '.btn-new' ).addEventListener( 'click', init );
@@ -81,9 +111,11 @@ function init() {
 	scores = [0, 0];
 	activePlayer = 0;
 	roundScore = 0;
+	gamePlaying = true;
 
-	document.querySelector( '.dice' ).style.display = 'none';
-	// getElementById is a bit faster than querySelector
+	document.getElementById( 'dice-1' ).style.display = 'none';
+	document.getElementById( 'dice-2' ).style.display = 'none';
+// getElementById is a bit faster than querySelector
 	document.getElementById( 'score-0' ).textContent = '0';
 	document.getElementById( 'score-1' ).textContent = '0';
 	document.getElementById( 'current-0' ).textContent = '0';
@@ -93,9 +125,15 @@ function init() {
 	document.querySelector( '.player-0-panel' ).classList.remove('winner');
 	document.querySelector( '.player-1-panel' ).classList.remove('winner');
 	document.querySelector( '.player-0-panel' ).classList.remove('active');
-	document.querySelector( '.player-0-panel' ).classList.add('active');
 	document.querySelector( '.player-1-panel' ).classList.remove('active');
+	document.querySelector( '.player-0-panel' ).classList.add('active');
 }
+
+// 1. A player losses his score if he hits two 6 in a row then the other player plays
+
+// 2. Add input for the predefined winning score so players can change it
+
+// 3. Add a 2nd dice, the player will loose his score if any of the dices hits 1
 
 // document.querySelector( '#current-' + activePlayer ).textContent = dice;
 // document.querySelector( '#current-' + activePlayer ).innerHTML = '<em>' + dice + '</em>';
